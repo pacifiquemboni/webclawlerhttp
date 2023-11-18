460bfcf984bd77b8bed85fc0cc5699c1756863f0
G O A T PC <pacifiquemboni>
2023-11-18 20:22:48 +0200

    printreport finished

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const { crawlPage} = require('./crawl.js')
+const { printReport} = require('./report.js')
 async function main(){
     if(process.argv.length<3) {
         console.log('no website provided')
@@ -11,9 +12,7 @@ async function main(){
     const baseURL = process.argv[2]
     console.log(`starting crawl of ${baseURL} `)
      const pages = await crawlPage(baseURL, baseURL, {})
-     for (const page of Object.entries(pages)){
-       console.log(page) 
-     }
+     printReport(pages)
 }
 
 main()
